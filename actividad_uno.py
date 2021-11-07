7c9bb2b59214eafa6cc8d679c0b61799c8ebaf02
MFerzzz <91223563+MFerzzz@users.noreply.github.com>
2021-11-07 11:46:25 -0500

    formulario

---
+++ actividad_uno.py
@@ -0,0 +1,8 @@
+"""Actividad N 1 
+Formulario en python"""
+#Declarando variables nombre, apellido, correo, dirección, edad, fecha_nacimiento
+nombre = input('cual es el nombre? :')
+apellido = input('cual es el apellido? :')
+edad = int(input('cual es tu edad :'))
+#imprimiendo datos del formulario
+print(nombre, apellido, edad)
